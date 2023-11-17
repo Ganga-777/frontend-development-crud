270ace6b2d97ffdd7acc6c200617e65c7718f2ef
Ganga-777 <144657923+Ganga-777@users.noreply.github.com>
2023-11-17 18:08:08 +0530

    Update EditStudent.js

--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -9,7 +9,7 @@ function EditStudent() {
     const [newData, setNewData] = useState([]);
 
     useEffect(() => {
-        Axios.get("https://crud-deplyoment-backend1.onrender.com/studentRoute/update-student/" + id)
+        Axios.get("https://backend-uvol.onrender.com/studentRoute/update-student/" + id)
             .then((res) => {
                 if (res.status === 200) {
                     const { name, email, rollNo } = res.data;
@@ -27,7 +27,7 @@ function EditStudent() {
 
     const handleSubmit = () => {
         const data = { name: newData[0], email: newData[1], rollNo: newData[2] };
-        Axios.put("https://crud-deplyoment-backend1.onrender.com/studentRoute/update-student/" + id, data)
+        Axios.put("https://backend-uvol.onrender.com/studentRoute/update-student/" + id, data)
             .then((res) => {
                 if (res.status === 200)
                     alert("Record updated successfully")
